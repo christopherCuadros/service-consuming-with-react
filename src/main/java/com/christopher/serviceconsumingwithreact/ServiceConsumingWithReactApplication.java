bcd09b91c073e63189ca1252fe423859d616ff34
christopher-cuadros <110701105+visual-junior@users.noreply.github.com>
2023-07-20 20:02:06 -0500

    inicio del repositorio

---
+++ src/main/java/com/christopher/serviceconsumingwithreact/ServiceConsumingWithReactApplication.java
@@ -0,0 +1,13 @@
+package com.christopher.serviceconsumingwithreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConsumingWithReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConsumingWithReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christopher/serviceconsumingwithreact/ServiceConsumingWithReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christopher.serviceconsumingwithreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConsumingWithReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
